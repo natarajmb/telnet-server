3898617590a10d73e12476057ff58458709020e8
Nataraj Basappa <nataraj.basappa@yahoo.com>
2014-01-16 20:27:57 +0000

    Updated the licence notice on the files

--- src/main/java/com/cognitivenode/telnet/command/CommandFactory.java
+++ src/main/java/com/cognitivenode/telnet/command/CommandFactory.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Cognitivenode 2013. All Rights Reserved.
+ *
+ * No part of this source code or any of its contents may be reproduced, copied,
+ * modified or adapted, without the prior written consent of the author,
+ * unless otherwise indicated for stand-alone materials.
+ */
+
 package com.cognitivenode.telnet.command;
 
 import org.slf4j.Logger;
